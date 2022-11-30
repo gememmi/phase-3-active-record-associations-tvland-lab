05bfbfe5dd6c7a7e98af476b0868a5326e43167a
Emily <emelizadaniels@gmail.com>
2022-11-30 13:59:09 -0500

    commit tree 56aeb4b0b54d6ee0eb2a1013b31fdc7ce19e5a71

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,13 @@
 class Character < ActiveRecord::Base
-  
+  belongs_to :actor
+  belongs_to :show
+
+  def actor 
+    self.actor
+  end
+
 end
+
+
+#actor: should return the actor instance this character belongs to.
+#  Hint: is there an association macro you can use to create this method?
